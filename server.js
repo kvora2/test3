42c5d2c4c13c4228aeed23825ecff2d0e62ae36b
kvora2 <kelvinvora21@gmail.com>
2022-11-23 17:51:30 -0500

    commit

---
+++ server.js
@@ -0,0 +1,90 @@
+/*************************************************************************
+* BTI325 – Test - 3
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+No part of this assignment has been copied manually or electronically from any other source.
+* (including 3rd party web sites) or distributed to other students.
+*
+* Name: Kelvin Vora Student ID: 157616210 Date: 19th Oct 2022
+*
+* Your app’s URL (from Cyclic Heroku) that I can click to see your application:
+* https://brainy-tux-bat.cyclic.app/
+*
+*************************************************************************/
+
+var express = require("express");
+var exphbs = require('express-handlebars');
+
+var app = express();
+
+var HTTP_PORT = process.env.PORT || 8080;
+var data = require("./test2_moduleB.js");
+
+function onHttpStart() {
+    console.log("Express http server listening on: " + HTTP_PORT);
+}
+
+app.engine('.hbs', exphbs.engine({
+    extname: ".hbs",
+    defaultLayout: "main",
+    helpers: {
+        navLink: function (url, options) {
+            return '<li' +
+                ((url == app.locals.activeRoute) ? ' class="active" ' : '') +
+                '><a href=" ' + url + ' ">' + options.fn(this) + '</a></li>';
+        },
+        equal: function (lvalue, rvalue, options) {
+            if (arguments.length < 3)
+                throw new Error("Handlebars Helper equal needs 2 parameters");
+            if (lvalue != rvalue) {
+                return options.inverse(this);
+            } else {
+                return options.fn(this);
+            }
+        }
+    }
+}));
+app.set("view engine", ".hbs");
+
+app.use(function (req, res, next) {
+    let route = req.baseUrl + req.path;
+    app.locals.activeRoute = (route == "/") ? "/" : route.replace(/\/$/, "");
+    next();
+})
+
+app.get("/", function (req, res) {
+    res.render('home');
+});
+
+app.get("/BSD", function (req, res) {
+    data.getBSD().then((data) => {
+        res.render("students", { students: data });
+    }).catch((err) => {
+        res.json({ ERROR: err });
+    })
+});
+
+app.get("/highGPA", function (req, res) {
+    data.highGPA().then((data) => {
+        res.render("student", { student: data });
+    }).catch((err) => {
+        res.json({ ERROR: err });
+    })
+})
+
+app.get("/allStudents", function (req, res) {
+    data.allStudents().then((data) => {
+        res.render("students", { students: data });
+    }).catch((err) => {
+        res.json({ ERROR: err });
+    })
+})
+
+app.get("*", function (req, res) {
+    res.send("Error 404: page not found.");
+})
+
+data.prepare().then(() => {
+    app.listen(HTTP_PORT, onHttpStart);
+}).catch((err) => {
+    console.log(err);
+})
---
+++ test2_moduleB.js
@@ -0,0 +1,84 @@
+const fs = require('fs');
+
+var students = []; // declaring global students variable
+
+// In the below function, students var will be populated if the file is readable
+exports.prepare = () => {
+    fs.readFile('./students.json', (err, data) => {
+        if (err) reject("fail to read the file students.json!");
+        students = JSON.parse(data);
+    })
+
+    return new Promise((resolve, reject) => {
+        if (!fs) {
+            reject("unable to read the file");
+        }
+        else {
+            resolve("Success!!");
+        }
+    })
+}
+
+exports.getCPA = () => {
+    return new Promise((res, rej) => {
+        let cpa_stud = [];
+        if (students.length != 0) {
+            for (let i = 0; i < students.length; i++) {
+                if (students[i].program === "CPA") {
+                    cpa_stud.push(students[i]);
+                }
+            }
+        }
+        if (students.length === 0 || cpa_stud.length === 0) {
+            rej("no results returned!");
+        }
+        else {
+            res(cpa_stud);
+        }
+    })
+}
+
+exports.getBSD = () => {
+    return new Promise((res, rej) => {
+        let bsd_stud = [];
+        if (students.length != 0) {
+            for (let i = 0; i < students.length; i++) {
+                if (students[i].program === "BSD") {
+                    bsd_stud.push(students[i]);
+                }
+            }
+        }
+        if (students.length === 0 || bsd_stud.length === 0) {
+            rej("no results returned!");
+        }
+        else {
+            res(bsd_stud);
+        }
+    })
+}
+
+exports.highGPA = () => {
+    return new Promise((res, rej) => {
+        let high_gpa = [];
+        high_gpa[0] = students[0];
+        if (students.length != 0) {
+            for (let i = 1; i < students.length; i++) {
+                if (high_gpa[0].gpa < students[i].gpa) {
+                    high_gpa[0] = students[i];
+                }
+            }
+        }
+        res(high_gpa);
+    })
+}
+
+exports.allStudents = () => {
+    return new Promise((res, rej) => {
+        if (students.length === 0) {
+            rej("no results Found!");
+        }
+        else {
+            res(students);
+        }
+    })
+}
